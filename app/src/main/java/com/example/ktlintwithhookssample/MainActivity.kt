574e13edbb97abf27445737839a82b81e304c5fc
bahaagabal <bgabal93@gmail.com>
2025-12-02 06:35:55 +0200

    test ktlin with pre commit

--- app/src/main/java/com/example/ktlintwithhookssample/MainActivity.kt
+++ app/src/main/java/com/example/ktlintwithhookssample/MainActivity.kt
@@ -11,18 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.ktlintwithhookssample.ui.theme.KtlintWithHooksSampleTheme
+import com.example.ktlintwithhookssample.ui.theme.ktlintWithHooksSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KtlintWithHooksSampleTheme {
+            ktlintWithHooksSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -31,17 +31,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    KtlintWithHooksSampleTheme {
-        Greeting("Android")
+fun greetingPreview() {
+    ktlintWithHooksSampleTheme {
+        greeting("Android")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "KtlintWithHooksSample"
 include(":app")
- 
